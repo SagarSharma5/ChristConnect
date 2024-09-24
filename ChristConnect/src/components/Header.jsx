3d4863f89a3def77588b951008b253defe3835ae
sagarsharma5 <sagar.sharma.5235@gmail.com>
2024-09-24 10:04:54 +0530

    add icon functionality

--- ChristConnect/src/components/Header.jsx
+++ ChristConnect/src/components/Header.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className="flex flex-row justify-center items-center text-white  border-b-[1px] border-black bg-darkblue">
       <div className="flex flex-row justify-center items-center gap-x-[780px]">
         <nav className="container  flex  items-center">
-          <div className="flex items-center">
+          <Link to="/" className="flex items-center">
             <img
               src="\finalchristlogo.png"
               alt=""
               className="h-[110px] w-60 cursor-pointer"
             />
-          </div>
+          </Link>
         </nav>
         <div className="">
           <SearchBar />
